3dfc3918048bd0e405dedd0a684894f59f1a578b
oscarsalomon89 <oscar.salomon89@gmail.com>
2022-11-16 09:23:13 -0300

    bson

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	_ "log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
@@ -30,9 +31,22 @@ type ItemRequestBody struct {
 	Photos      []string `json:"photos"`
 }
 
+type Item struct {
+	Code        string     `bson:"code" json:"code"`
+	Title       string     `bson:"title" json:"title"`
+	Description string     `bson:"description" json:"description"`
+	Price       int        `bson:"price" json:"price"`
+	Stock       int        `bson:"stock" json:"stock"`
+	Photos      []string   `bson:"photos" json:"photos"`
+	Status      string     `bson:"status" json:"status"`
+	CreatedAt   *time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt   *time.Time `bson:"updated_at" json:"updated_at"`
+}
+
 type ItemResponse struct {
-	ID string `json:"id"`
-	ItemRequestBody
+	Status int    `json:"status"`
+	ID     string `json:"id"`
+	Data   Item   `json:"data"`
 }
 
 func main() {
@@ -98,7 +112,28 @@ func createItemHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	id, err := createRecord(req.Context(), data)
+	var status string
+	if status = "active"; data.Stock == 0 {
+		status = "inactive"
+	}
+
+	timeNow := time.Now()
+
+	itemToSave := Item{
+		Code:        data.Code,
+		Title:       data.Title,
+		Description: data.Description,
+		Price:       data.Price,
+		Stock:       data.Stock,
+		Photos:      data.Photos,
+		Status:      status,
+		CreatedAt:   &timeNow,
+		UpdatedAt:   &timeNow,
+	}
+
+	collection := client.Database(dbName).Collection("items")
+
+	result, err := collection.InsertOne(req.Context(), itemToSave)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(struct {
@@ -111,20 +146,17 @@ func createItemHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	id := fmt.Sprintf("%v", result.InsertedID)
+
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(struct {
-		Status int
-		Data   ItemResponse
-	}{
+	json.NewEncoder(w).Encode(ItemResponse{
 		Status: http.StatusOK,
-		Data: ItemResponse{
-			ID:              id,
-			ItemRequestBody: *data,
-		},
+		ID:     id,
+		Data:   itemToSave,
 	})
 }
 
-func getRecords(ctx context.Context) ([]ItemRequestBody, error) {
+func getRecords(ctx context.Context) ([]Item, error) {
 	collection := client.Database(dbName).Collection("items")
 
 	cursor, err := collection.Find(ctx, bson.D{})
@@ -133,10 +165,10 @@ func getRecords(ctx context.Context) ([]ItemRequestBody, error) {
 	}
 	defer cursor.Close(ctx)
 
-	var items []ItemRequestBody
+	var items []Item
 
 	for cursor.Next(ctx) {
-		var item ItemRequestBody
+		var item Item
 
 		if err = cursor.Decode(&item); err != nil {
 			return nil, err
@@ -146,15 +178,4 @@ func getRecords(ctx context.Context) ([]ItemRequestBody, error) {
 	}
 
 	return items, nil
-}
-
-func createRecord(ctx context.Context, data *ItemRequestBody) (string, error) {
-	collection := client.Database(dbName).Collection("items")
-
-	req, err := collection.InsertOne(ctx, data)
-	if err != nil {
-		return "", err
-	}
-
-	return fmt.Sprintf("%v", req.InsertedID), nil
 }
